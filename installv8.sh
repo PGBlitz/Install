52e4caf918f5ffbe844e142e3178c8be7ec6fab9
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-02-21 06:47:01 -0500

    First Push

---
+++ installv8.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#
+# Title:      PlexGuide (Reference Title File)
+# Author(s):  Admin9705 - Deiteq
+# URL:        https://plexguide.com - http://github.plexguide.com
+# GNU:        General Public License v3.0
+################################################################################
+
+# Upgrade
+apt-get update -y
+# apt-get upgrade -y # causes problems when you forced on an interactive screen
+apt-get install software-properties-common git zip unzip dialog -y
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🌎  INSTALLING: PlexGuide Notice
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+By Installing PlexGuide, you are agreeing to the terms and conditions
+of the GNUv3 Project License! Please Standby...
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+sleep 3
+
+# Delete If it Exist for Cloning
+file="/opt/plexguide"
+if [ -e "$file" ]; then rm -rf /opt/plexguide; fi
+
+file="/opt/pgstage"
+if [ -e "$file" ]; then rm -rf /opt/pgstage; fi
+
+rm -rf /opt/pgstage/place.holder 1>/dev/null 2>&1
+
+git clone -b v8 --single-branch https://github.com/Admin9705/PlexGuide-Installer.git /opt/pgstage
+
+mkdir -p /var/plexguide/logs
+echo "127.0.0.1:" > /var/plexguide/server.ports
+echo "51" > /var/plexguide/pg.pythonstart
+touch /var/plexguide/pg.pythonstart.stored
+start=$(cat /var/plexguide/pg.pythonstart)
+stored=$(cat /var/plexguide/pg.pythonstart.stored)
+
+if [ "$start" != "$stored" ]; then
+bash /opt/pgstage/pyansible.sh
+fi
+echo "51" > /var/plexguide/pg.pythonstart.stored
+
+ansible-playbook /opt/pgstage/clone.yml
+cp /opt/plexguide/menu/alias/templates/plexguide /bin/plexguide
+
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⌛  Verifiying PlexGuide Installed @ /bin/plexguide - Please Standby!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 2
+
+file="/bin/plexguide"
+if [ ! -e "$file" ]; then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️  WARNING! Installed Failed! PlexGuide Command Missing!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+Please Reinstall PlexGuide by running the Command Again! We are doing
+this to ensure that your installation continues to work!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+exit
+fi
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅️  PASSED! The PlexGuide Command Installed!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+rm -rf /var/plexguide/new.install 1>/dev/null 2>&1
+sleep 2
+chmod 755 /bin/plexguide
+chown 1000:1000 /bin/plexguide
+
+## Other Folders
+mkdir -p /opt/appdata/plexguide
+mkdir -p /var/plexguide
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+↘️  Start AnyTime By Typing >>> plexguide
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
---
+++ versions.sh
@@ -0,0 +1,10 @@
+8.4.20
+8.5-beta3
+###########################-VERSIONS-PRIOR
+8.4.18
+###### DONT RECOMMEND BELOW (ARCHIEVED VERSIONS)
+8.3.17
+###########################-COMMANDS-BELOW-FOR-TESTING
+next
+v8.4base
+v8.5base
