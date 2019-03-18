07b1f293d2f2ac29f0b3274258b1124ced06730a
Robert Baker <se7enalive@gmail.com>
2019-03-17 19:01:37 -0700

    Revert "Update versions.sh"
    This reverts commit a9b9f894bc38a795db9bdfaffcec8b402ad46f9a.

--- versions.sh
+++ versions.sh
@@ -1,7 +1,14 @@
-8.6test9
-v8.6base
+8.5.8
+8.6test6
 ###########################-VERSIONS-PRIOR
-8.6test8
+8.6test5
+8.5.7
+8.5.4
+8.5.0
 ###### DONT RECOMMEND BELOW (ARCHIEVED VERSIONS)
+8.4.20
+8.4.18
+8.3.17
 ###########################-COMMANDS-BELOW-FOR-TESTING
 v8.6base
+v8.5base
