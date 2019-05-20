2fe027191ffdfadeae41d1ed151664b9d6d17e9a
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-05-20 14:50:12 -0400

    update

--- versions.sh
+++ versions.sh
@@ -1,15 +1,7 @@
-8.6.3
-v8.6base
-###########################-VERSIONS-PRIOR
-8.6.2
-8.6.1
-8.6RC1
-8.5.8
-###### DONT RECOMMEND BELOW (ARCHIEVED VERSIONS)
-8.5.7
-8.5.4
-8.4.20
-8.4.18
-8.3.17
-###########################-COMMANDS-BELOW-FOR-TESTING
-v9.0base
+9.0.2
+#1 ######################### VERSIONS-PRIOR
+9.0.1
+#2 #### DONT RECOMMEND BELOW (ARCHIEVED VERSIONS)
+9.0.0
+#3 ###########################-COMMANDS-BELOW-FOR-TESTING
+base9.0
